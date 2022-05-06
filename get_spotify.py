c6fcce73117c592cabb2dae45e6c5b3c20026881
KaileYing <835741923@qq.com>
2022-05-05 23:03:12 -0400

    `1`

--- get_spotify.py
+++ get_spotify.py
@@ -2,15 +2,14 @@ from os import access
 import requests
 import random
 
-ACCESS_TOKEN = 'BQBYJDvdIJBgH0VBwXMFlM6m2QNPsrR0aFFxSIWTuVE-P-ueyPOZZSEP-pH51C_PjqO4uBhHiiXk78dFYkij5OUApw7KWq12wK9aHHuXtJGgwNdnn05gTHMZrgmE0KjTLgFMlfFiMbRZqdDsLxUQOoI_pqoiXmdg2lYG2wI'
+ACCESS_TOKEN = 'BQBEyYaFcQetSplzP6W_GIKoVqfrXAl1YqXk9TZ_PJwrTdYDPoEXh9m792Ffctw2YntWWA3xr-L5Q_N0j3WiYxQx81Qg3dEjVxw7x6BSRElgYo5Duws_OXwAfbv5yqfWYQ3uKbyEWz0YcKCaPCbtVnZLy2Ncp4yQTcqKaAU'
 
 
 
 def get_track():
-    num = random.randint(0,99)
+    num = random.randint(0,52)
     headers = {"Authorization" : f"Bearer {ACCESS_TOKEN}"}
     get_url = "https://api.spotify.com/v1/playlists/0hkMbHNNTtbyHOibUudH3g"
     response = requests.get(url=get_url, headers=headers)
     data = response.json()['tracks']['items'][num]['track']['external_urls']['spotify']
     return data
-
