dceaa52177e62b5b1b469fce9c714eb722e27dda
KaileYing <835741923@qq.com>
2022-06-01 19:40:46 +0000

    Created game recommandation function

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from Server import keep_alive
 import get_course
 from replit import db
 import time
+import csv
 
 load_dotenv('TOKEN.env')
 client = discord.Client()
@@ -34,7 +35,7 @@ async def on_message(message):
         await message.channel.send(rdsong)
 
     elif message.content.startswith('!help'):
-        await message.channel.send('【!hello】 : Say hello~ \n【!random song】 : Random song from Spotify (API restricted)\n【!今日运势】 : 查看今日运势 \n【!签到】 : 签到\n【!random video】 : Random video from Youtube \n【!c + course code】 : General Education courses information in Penn State.' )
+        await message.channel.send('【!hello】 : Say hello~ \n【!random song】 : Random song from Spotify (API restricted)\n【!random video】 : Random video from Youtube \n【!c + course code】 : General Education courses information in Penn State.\n【!今日运势】 : 查看今日运势 \n【!签到】 : 签到\n【!推荐游戏】: 随机推荐游戏' )
 
     elif message.content.startswith('!今日运势'):
         yi = str(things[random.randint(0,len(things)-1)]) + '】'
@@ -69,25 +70,35 @@ async def on_message(message):
             
     elif message.content.startswith('!签到'):
         id = str(message.author.id)
-        localtime = time.strftime("%d, time.localtime()")
+        localtime = time.strftime("%d",time.localtime())
+        print(localtime)
         info = []
         if id not in db.keys():
             info.append("1")
             info.append(localtime)
             db[id] = info
+            value_sign = db[id][0]
+            await message.channel.send("签到成功！\n已签到【" + value_sign + "】天")
             
         else:
-            if db[id][1] == localtime:
+            if db[id][1] == localtime:                
                 await message.channel.send("今日已经签到过了哟～\n签到天数:【" + db[id][0] + "】天")
             else:
-                days = int(db[id])
+                days = int(db[id][0])
                 days += 1
                 info.append(days)
                 info.append(localtime)
                 db[id] = info
         
-                value_sign = db[id][0]
+                value_sign = str(db[id][0])
                 await message.channel.send("签到成功！\n已签到【" + value_sign + "】天")
+
+    elif message.content.startswith('!推荐游戏'):
+        with open("./VideoGames.csv") as f:
+            reader = csv.reader(f)
+            result = list(reader)
+            result = result[random.randint(1,16718)]
+            await message.channel.send("游戏名称:【 " + result[0] + " 】\n平台:【 " + result[1] + " 】\n发布日期:【 " + result[2] + " 】\n类型:【 " + result[3] + " 】\n发布者:【 " + result[4] + " 】")
 
 try:
     keep_alive()
